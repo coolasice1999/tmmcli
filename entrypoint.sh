3ad7f34201d5859d946063124584c51119884113
coolasice1999 <52601736+coolasice1999@users.noreply.github.com>
2019-08-06 12:00:51 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ ! -f /mnt/cron.d/cron.conf ]; then
+if [ ! -f /mnt/cron.conf ]; then
     echo "0 5 * * * /config/tinyMediaManagerCMD.sh -updateMovies -updateTv -scrapeNew" > /mnt/cron.d/cron.conf
 fi
 
-chmod 600 /mnt/cron.d/cron.conf
-crontab /mnt/cron.d/cron.conf
+chmod 600 /mnt/cron.conf
+crontab /mnt/cron.conf
 
 exec "$@"
