fd2a6b1647bfa8ac92626ffa159c387c4fa8f89e
coolasice1999 <52601736+coolasice1999@users.noreply.github.com>
2019-08-06 06:44:25 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,6 @@ if [ ! -f /mnt/cron.d/cron.conf ]; then
     echo "* * * * * echo 'Running'" > /mnt/cron.d/cron.conf
 fi
 
-ENV_VARS='$ENV_DOMAIN_NAME:$ENV_UPSTREAM_HOST:$ENV_SMTP_HOST';
-
-envsubst "$ENV_VARS" \
-    < /mnt/ssmtp.template \
-    > /etc/ssmtp/ssmtp.conf
-
 chmod 600 /mnt/cron.d/cron.conf
 crontab /mnt/cron.d/cron.conf
 
