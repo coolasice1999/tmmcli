bfceada157873ad9f571c86c199249c15beeadfb
coolasice1999 <52601736+coolasice1999@users.noreply.github.com>
2020-05-06 14:08:31 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /mnt/cron.conf ]; then
 #set time to 12:30AM run
-    echo "$Cron_Timing /config/tinyMediaManagerCMD.sh -updateMovies -updateTv -scrapeUnscraped" > /mnt/cron.conf
+    echo "30 0 * * * /config/tinyMediaManagerCMD.sh -updateMovies -updateTv -scrapeUnscraped" > /mnt/cron.conf
 fi
 chmod 777 /config/*
 chmod 600 /mnt/cron.conf
